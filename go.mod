5a92a332d88e349507b23901feda44ae78aa56d5
wdy <wdy0808@outlook.com>
2019-09-06 22:37:12 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wdy0808/go-common
+
+go 1.12
+
+require github.com/google/uuid v1.1.1
---
+++ uuid/v4.go
@@ -0,0 +1,22 @@
+package uuid
+
+import (
+	"github.com/google/uuid"
+	"github.com/wdy0808/go-common/log"
+)
+
+func NewRandomUuid() (newUuid string, err error) {
+	id, err := uuid.NewRandom()
+	time := 0
+	for err != nil {
+		id, err = uuid.NewRandom()
+		time++
+		if time >= retryTime {
+			log.LogError("create random uuid exceed max retry time [%d], last error message [%s]", retryTime, err.Error())
+			panic("random uuid generation error")
+		}
+	}
+
+	newUuid = id.String()
+	return
+}
